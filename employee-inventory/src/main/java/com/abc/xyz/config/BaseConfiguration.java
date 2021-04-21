e59f4df8c9af2dead5e47145d8e32b48e3e3b083
vinodyarragonda <vinod.reddy8747@gmail.com>
2021-04-21 12:22:23 +0530

    Adding swagger configuration

--- employee-inventory/src/main/java/com/abc/xyz/config/BaseConfiguration.java
+++ employee-inventory/src/main/java/com/abc/xyz/config/BaseConfiguration.java
@@ -11,8 +11,16 @@ import org.springframework.context.annotation.Primary;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
 @EnableBatchProcessing
+@EnableSwagger2
 public class BaseConfiguration {
 
 	@Primary
@@ -31,5 +39,14 @@ public class BaseConfiguration {
 		config.setPassword(password);
 		return new HikariDataSource(config);
 	}
-
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+		
 }
